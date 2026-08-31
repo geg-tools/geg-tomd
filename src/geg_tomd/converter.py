65f9e73cefda5bc787c65bdd16592ce114563416
Gabriel Guerra <gabrielcake9@gmail.com>
2026-08-31 17:16:13 -0300

    adiciona funcionalidade de completar texto resumido e extração de texto de PDF sem I.A.

--- src/geg_tomd/converter.py
+++ src/geg_tomd/converter.py
@@ -1,17 +1,27 @@
 from pathlib import Path
 
+import pymupdf4llm
 from geg_core.files import detect_file_extension
+
 from geg_tomd.llm import GeminiService
 
-def convert(file_path: Path, summarize: bool, use_ai: bool, gemini_service: GeminiService | None) -> str:
+
+def convert(
+    file_path: Path,
+    summarize: bool,
+    use_ai: bool,
+    complete: bool,
+    gemini_service: GeminiService | None,
+) -> str:
     file_extension = detect_file_extension(file_path)
 
-    # deixar gemini service generico e passar prompt e arquivo como parametro
+    # PDF
     if file_extension in (".pdf"):
         if use_ai:
             md_text = gemini_service.extract_pdf_text(file_path=str(file_path))
         else:
-            md_text = "sem ia"
+            md_text = pymupdf4llm.to_markdown(str(file_path))
+    # TEXT FILES
     elif file_extension in (".txt", ".md"):
         md_text = file_path.read_text(encoding="utf-8")
     else:
@@ -20,9 +30,16 @@ def convert(file_path: Path, summarize: bool, use_ai: bool, gemini_service: Gemi
 
     print("texto gerado...")
 
-    # resume o texto gerado
+    # COMPLETE
+    if complete:
+        md_text = gemini_service.complete(text=md_text)
+        print("texto preenchido...")
+
+    # RESUME
     if summarize:
         md_text = gemini_service.summarize(text=md_text)
         print("texto resumido...")
+
+    print(f"Resultado escrito em {file_path}")
 
     return md_text
